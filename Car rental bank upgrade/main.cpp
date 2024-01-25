0a613772cecf731c72fb9f9f03fab2098cc3007b
Dorian11ro-coding <minimickey427@yahoo.com>
2024-01-25 19:52:54 +0200

    Initial Commit

---
+++ Car rental bank upgrade/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Car rental bank upgrade
+//
+//  Created by Scarlat  Dorian Andrei on 25.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
